873b825f95b4dcfaca66f1be81ebd2106c6a3a56
Nowruz Owliyagulyyew <161121707+Novruz3@users.noreply.github.com>
2025-11-12 02:46:46 +0500

    add 2 problem easy

--- island-perimeter.js
+++ island-perimeter.js
@@ -37,3 +37,29 @@ var islandPerimeter = function (grid) {
   }
   return p;
 };
+
+
+// best solutiion 0ms
+var islandPerimeter = function (grid){
+  const rows=grid.length;
+  const cols=grid[0].length;
+  let perimeter=0;    
+
+  for(let i=0;i<rows;i++){
+    for(let j=0;j<cols;j++){
+      if(grid[i][j]===1){
+        perimeter+=4;
+        if(i>0 && grid[i-1][j]===1){
+          perimeter-=2;
+        }
+        if(j>0 && grid[i][j-1]===1){
+          perimeter-=2;
+        }
+        
+      }
+    }
+
+
+}
+  return perimeter;
+}
